6075470efbf058fe7900e822ab5e76ff0feee8c8
Thiago Vieira <thiagovieira7@outlook.com>
2022-12-21 21:23:51 -0300

    inicio projeto aula

---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Home from "./components/Home";
+import "./index.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Home />
+    <Home />
+    <Home />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
