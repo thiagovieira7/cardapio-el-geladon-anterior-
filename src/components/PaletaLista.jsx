b9bfb427d9157306ffcffbc1d63d9699449181c3
Thiago Vieira <thiagovieira7@outlook.com>
2023-01-11 15:29:26 -0300

    setTimeout

--- src/components/PaletaLista.jsx
+++ src/components/PaletaLista.jsx
@@ -1,14 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "./PaletaLista.css";
 import { paletas } from "../mocks/paletas.js";
 
 function PaletaLista() {
-  
   const [paletaSelecionada, setPaletaSelecionada] = useState({});
 
-  const adicionarItem = (paletaIndex) => { 
-    
-  }
+  const adicionarItem = (paletaIndex) => {
+    const paleta = {
+      [paletaIndex]: Number(paletaSelecionada[paletaIndex] || 0) + 1,
+    };
+    setPaletaSelecionada({ ...paletaSelecionada, ...paleta });
+  };
 
   return (
     <div className="PaletaLista">
@@ -21,7 +23,10 @@ function PaletaLista() {
             </div>
             <div className="PaletaListaItem__descricao">{paleta.descricao}</div>
             <div className="PaletaListaItem__acoes Acoes">
-              <button className="Acoes__adicionar Acoes__adicionar--preencher">
+              <button
+                className="Acoes__adicionar Acoes__adicionar--preencher"
+                onClick={() => adicionarItem(index)}
+              >
                 adicionar
               </button>
             </div>
