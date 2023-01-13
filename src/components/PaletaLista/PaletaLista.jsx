349573fb6c9cdff4caca3776d6ae16c86122f940
thiagovieira7 <thiagovieira7@outlook.com>
2023-01-13 16:31:57 -0300

    subdivisão de componentes

--- src/components/PaletaLista/PaletaLista.jsx
+++ src/components/PaletaLista/PaletaLista.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import PaletaListaItem from 'components/PaletaListaItem/PaletaListaItem'
 import "./PaletaLista.css";
 import { paletas } from "mocks/paletas.js";
 
@@ -19,51 +20,13 @@ function PaletaLista() {
     setPaletaSelecionada({ ...paletaSelecionada, ...paleta });
   };
 
-  const badgeCounter = (canRender, index) =>
-    Boolean(canRender) && (
-      <span className="PaletaListaItem__badge">
-        {" "}
-        {paletaSelecionada[index]}{" "}
-      </span>
-    );
 
-  const removeButton = (canRender, index) =>
-    Boolean(canRender) && (
-      <button className="Acoes_remover" onClick={() => removerItem(index)}>
-        remover
-      </button>
-    );
 
   return (
     <div className="PaletaLista">
       {paletas.map((paleta, index) => (
-        <div className="PaletaListaItem" key={`PaletaListaItem-${index}`}>
-          {badgeCounter(paletaSelecionada[index], index)}
-          <div>
-            <div className="PaletaListaItem__titulo">{paleta.titulo}</div>
-            <div className="PaletaListaItem__preco">
-              {paleta.preco.toFixed(2)}
-            </div>
-            <div className="PaletaListaItem__descricao">{paleta.descricao}</div>
-            <div className="PaletaListaItem__acoes Acoes">
-              <button
-                className={`Acoes__adicionar ${
-                  !paletaSelecionada[index] && "Acoes__adicionar--preencher"
-                }`}
-                onClick={() => adicionarItem(index)}
-              >
-                adicionar
-              </button>
-              {removeButton(paletaSelecionada[index], index)}
-            </div>
-          </div>
-          <img
-            className="PaletaListaItem__foto"
-            src={paleta.foto} //cuidar com camimnho da imagem, pois parrou tudo....
-            // eslint-disable-next-line no-template-curly-in-string
-            alt={"Paleta de ${paleta.sabor}"}
-          />
-        </div>
+        <PaletaListaItem key={`PaletaListaItem-${index}`}/>
+
       ))}
     </div>
   );
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,34 +1,11 @@
 import "./Home.css";
 import PaletaLista from "components/PaletaLista/PaletaLista";
-import sacola from "assets/icons/sacola.svg";
-import logo from "assets/logo.svg";
+import Navbar from "components/Navbar/Navbar";
 
 function Home() {
   return (
     <div className="Home">
-      <div className="Home__header Header">
-        <div className="row">
-          <div className="Header__logo Logo">
-            <img
-              src={logo}
-              width="70px"
-              alt="Logo El Geladon"
-              className="Logo__icone"
-            />
-            <span className="Logo__titulo"> El Geladon </span>
-          </div>
-          <div className="Header__opcoes Opcoes">
-            <div className="Opcoes__sacola Sacola">
-              <img
-                src={sacola}
-                width="40px"
-                className="Sacola__icone"
-                alt="Sacola de compras"
-              />
-            </div>
-          </div>
-        </div>
-      </div>
+      <Navbar/>
       <div className="Home__container">
         <PaletaLista />
       </div>
